da97f09f98113dea9ecee71620b293e59d5feaf2
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-11-23 17:04:44 +0530

    singup and signup routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,79 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign, verify } from "hono/jwt";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use("/api/v1/blog/*", async (c, next) => {
+  const header = c.req.header("Authorization") || "";
+  const response = await verify(header, "secret");
 
-export default app
+  if (response.id) {
+    next();
+  } else {
+    c.status(403);
+    return c.json({
+      error: "unauthorized",
+    });
+  }
+});
+
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const { email, password } = await c.req.json();
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password,
+    },
+  });
+
+  const token = await sign(
+    {
+      id: user.id,
+    },
+    "secret"
+  );
+
+  return c.json({
+    jwt: token,
+  });
+});
+
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const { email, password } = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+      password,
+    },
+  });
+
+  return c.json({
+    data: user,
+    msg: "signed in successfully",
+  });
+});
+app.post("/api/v1/blog", (c) => {
+  return c.text("Blog added");
+});
+app.put("/api/v1/blog", (c) => {
+  return c.text("blog update");
+});
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("signup");
+});
+
+export default app;
