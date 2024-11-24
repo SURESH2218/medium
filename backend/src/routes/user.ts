b2976e3263455098b4d351355c31e7aec5667cc6
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-11-24 18:45:01 +0530

    user controller

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,55 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign, verify } from "hono/jwt";
+
+export const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+userRouter.post("/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const { email, password } = await c.req.json();
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password,
+    },
+  });
+
+  const token = await sign(
+    {
+      id: user.id,
+    },
+    c.env.JWT_SECRET
+  );
+
+  return c.json({
+    jwt: token,
+  });
+});
+
+userRouter.post("/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const { email, password } = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+      password,
+    },
+  });
+
+  return c.json({
+    data: user,
+    msg: "signed in successfully",
+  });
+});
