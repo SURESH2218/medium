3b0e69be4670cb21471a6edb6f196790c87f510b
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-11-24 18:44:25 +0530

    adding router to the app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,79 +1,11 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign, verify } from "hono/jwt";
+import { verify } from "hono/jwt";
+import { userRouter } from "./routes/user";
+import { blogRouter } from "./routes/blog";
 
-const app = new Hono<{
-  Bindings: {
-    DATABASE_URL: string;
-  };
-}>();
+const app = new Hono();
 
-app.use("/api/v1/blog/*", async (c, next) => {
-  const header = c.req.header("Authorization") || "";
-  const response = await verify(header, "secret");
-
-  if (response.id) {
-    next();
-  } else {
-    c.status(403);
-    return c.json({
-      error: "unauthorized",
-    });
-  }
-});
-
-app.post("/api/v1/signup", async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  const { email, password } = await c.req.json();
-  const user = await prisma.user.create({
-    data: {
-      email,
-      password,
-    },
-  });
-
-  const token = await sign(
-    {
-      id: user.id,
-    },
-    "secret"
-  );
-
-  return c.json({
-    jwt: token,
-  });
-});
-
-app.post("/api/v1/signin", async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  const { email, password } = await c.req.json();
-  const user = await prisma.user.findUnique({
-    where: {
-      email,
-      password,
-    },
-  });
-
-  return c.json({
-    data: user,
-    msg: "signed in successfully",
-  });
-});
-app.post("/api/v1/blog", (c) => {
-  return c.text("Blog added");
-});
-app.put("/api/v1/blog", (c) => {
-  return c.text("blog update");
-});
-app.get("/api/v1/blog/:id", (c) => {
-  return c.text("signup");
-});
+app.route("api/v1/user", userRouter);
+app.route("api/v1/blog", blogRouter);
 
 export default app;
