e3998a30c2d524ed737baf98a2de085a27cd0474
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-11-25 14:10:39 +0530

    sending inputs from frontned

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,13 +1,31 @@
 import { ChangeEvent, useState } from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { SingUpInput } from "../../../common/src/zod";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 const Auth = ({ type }: { type: "signup" | "signin" }) => {
+  const navigate = useNavigate();
   const [postInputs, setpostInputs] = useState<SingUpInput>({
     name: "",
     email: "",
     password: "",
   });
+
+  async function sendRequest() {
+    try {
+      const response = await axios.post(
+        `${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`,
+        postInputs
+      );
+      const jwt = response.data;
+      localStorage.setItem("token", jwt);
+      navigate("/blogs");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -65,6 +83,7 @@ const Auth = ({ type }: { type: "signup" | "signin" }) => {
               }}
             />
             <button
+              onClick={sendRequest}
               type="button"
               className="mt-5 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
             >
---
+++ frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://backend.sureshalabani2785.workers.dev";
