67f27a31118cd7739135648321edb06aef676a84
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-11-25 13:28:56 +0530

    quote div

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,15 @@
+const Quote = () => {
+  return (
+    <div className="bg-slate-200 h-screen flex flex-col justify-center">
+      <div className="flex justify-center">
+        <div className="max-w-xl text-3xl">
+          "The customer support I received was exceptional.The support team was
+          above and beyond to address my concerns"
+        </div>
+      </div>
+      <div className="text-xl font-semibold text-center">---Suresh Alabani</div>
+    </div>
+  );
+};
+
+export default Quote;
