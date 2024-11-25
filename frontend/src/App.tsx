eba67344758901d368e05674578caea5a1fa99e4
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-11-25 13:27:36 +0530

    Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Blog from "./pages/Blog";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl">Hello World !!!!!!!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
